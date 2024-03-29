e0d87d41ab91e1d58ee9e384cc5171e31c8ed955
noahtheprogrammer <weavernoah11@gmail.com>
2024-03-28 23:41:37 -0400

    SL, TP, and position now stored in JSON file

--- soltrade/trading.py
+++ soltrade/trading.py
@@ -4,15 +4,17 @@ import pandas as pd
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
-from soltrade.transactions import perform_swap, MarketPosition
+from soltrade.transactions import perform_swap, market
 from soltrade.indicators import calculate_ema, calculate_rsi, calculate_bbands
 from soltrade.wallet import find_balance
 from soltrade.log import log_general, log_transaction
 from soltrade.config import config
 
+market('position.json')
 
 # Stoploss and trading values for statistics and algorithm
-stoploss = takeprofit = 0
+stoploss = market().sl
+takeprofit = market().tp
 ema_short = ema_medium = 0
 upper_bb = lower_bb = 0
 rsi = 0
@@ -30,17 +32,17 @@ def fetch_candlestick():
         exit()
     return response.json()
 
-
 # Analyzes the current market variables and determines trades
 def perform_analysis():
     log_general.debug("Soltrade is analyzing the market; no trade has been executed.")
 
-    global stoploss
-    global takeprofit
+    global stoploss, takeprofit
     global ema_short, ema_medium
     global rsi
     global price
     global upper_bb, lower_bb
+
+    market().load_position()
 
     # Converts JSON response for DataFrame manipulation
     candle_json = fetch_candlestick()
@@ -61,39 +63,46 @@ def perform_analysis():
     rsi = calculate_rsi(dataframe=df, length=14)
     upper_bb, lower_bb = calculate_bbands(dataframe=df, length=14)
 
-    if not MarketPosition().position:
+    if not market().position:
         usdc_balance = find_balance(config().usdc_mint)
         input_amount = round(usdc_balance, 1) - 0.01
         if (ema_short > ema_medium or price < lower_bb.iat[-1]) and rsi <= 31:
             log_transaction.info("Soltrade has detected a buy signal.")
             log_transaction.info(get_statistics())
             if input_amount <= 0 or input_amount >= usdc_balance:
-                log_transaction.info("Soltrade has detected a buy signal, but does not have enough USDC to trade.")
+                log_transaction.warning("Soltrade has detected a buy signal, but does not have enough USDC to trade.")
                 return
-            asyncio.run(perform_swap(input_amount, config().usdc_mint))
-            stoploss = cl.iat[-1] * 0.925
-            takeprofit = cl.iat[-1] * 1.25
+            is_swapped = asyncio.run(perform_swap(input_amount, config().usdc_mint))
+            if is_swapped:
+                stoploss = market().sl = cl.iat[-1] * 0.925
+                takeprofit = market().tp = cl.iat[-1] * 1.25
+                market().update_position(True, stoploss, takeprofit)
+            return
     else:
         input_amount = round(find_balance(config().other_mint), 1) - 0.01
 
         if price <= stoploss or price >= takeprofit:
             log_transaction.info("Soltrade has detected a sell signal. Stoploss or takeprofit has been reached.")
             log_transaction.info(get_statistics())
-            asyncio.run(perform_swap(input_amount, config().other_mint))
-            stoploss = takeprofit = 0
+            is_swapped = asyncio.run(perform_swap(input_amount, config().other_mint))
+            if is_swapped:
+                stoploss = takeprofit = market().sl = market().tp = 0
+                market().update_position(False, stoploss, takeprofit)
             return
 
         if (ema_short < ema_medium or price > upper_bb.iat[-1]) and rsi >= 68:
             log_transaction.info("Soltrade has detected a sell signal. EMA or BB has been reached.")
             log_transaction.info(get_statistics())
-            asyncio.run(perform_swap(input_amount, config().other_mint))
-            stoploss = takeprofit = 0
-
+            is_swapped = asyncio.run(perform_swap(input_amount, config().other_mint))
+            if is_swapped:
+                stoploss = takeprofit = market().sl = market().tp = 0
+                market().update_position(False, stoploss, takeprofit)
+            return
 
 # This starts the trading function on a timer
 def start_trading():
     log_general.info("Soltrade has now initialized the trading algorithm.")
-    log_general.debug("Available commands are /statistics, /pause, /resume, and /quit.")
+    log_general.debug("Available commands are statistics, pause, resume, and quit.")
 
     trading_sched = BackgroundScheduler()
     trading_sched.add_job(perform_analysis, 'interval', seconds=config().price_update_seconds, max_instances=1)
@@ -101,21 +110,20 @@ def start_trading():
     perform_analysis()
 
     while True:
-        event = input().lower()
-        if event == '/pause':
+        event = input()
+        if event == 'pause':
             trading_sched.pause()
             log_general.info("Soltrade has now been paused.")
-
-        if event == '/resume':
+        elif event == 'resume':
             trading_sched.resume()
             log_general.info("Soltrade has now been resumed.")
-        if event == '/statistics':
+        elif event == 'statistics':
             print_statistics()
-
-        if event == '/quit':
+        elif event == 'quit':
             log_general.info("Soltrade has now been shut down.")
             exit()
-
+        else:
+            log_general.warning("Soltrade does not recognize that command.")
 
 def get_statistics():
     return f"""
